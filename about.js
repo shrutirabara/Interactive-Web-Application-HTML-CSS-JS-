8198db8c9bad5af36088bd45220cfcf09a8e1ba4
Shruti Rabara <61359514+shrutirabara@users.noreply.github.com>
2021-12-18 23:15:44 -0500

    Add files via upload

---
+++ about.js
@@ -0,0 +1,16 @@
+//hello
+$(document).ready(function() {
+    $("#slider").bxSlider({
+        auto: true,
+        minSlides: 1,
+        maxSlides: 1,
+        slideWidth: 500,
+        slideMargin: 20,
+        randomStart: false,
+        captions: true,
+        pause: 3000,
+        pagerType: "short"
+
+    });
+    
+});
